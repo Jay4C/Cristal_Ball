58e148f1a39bf500c6ebb19ef3cf5c5b762de51e
Jason ALOYAU <jason.aloyau@outlook.fr>
2022-07-31 13:43:43 +0000

    AI _ KM _ DM _ Job _ Indeed _ test_extract_all_ad_link_from_all_pages_into_excel

--- Artificial_Intelligence/Knowledge_Management/Data_Mining/Job/Indeed/unit_tests_for_github.py
+++ Artificial_Intelligence/Knowledge_Management/Data_Mining/Job/Indeed/unit_tests_for_github.py
@@ -183,6 +183,48 @@ class UnitTestsDataMiningIndeed(unittest.TestCase):
 
         workbook.close()
 
+    def test_extract_all_ad_link_from_all_pages_into_excel(self):
+        print('test_extract_all_ad_link_from_all_pages_into_excel')
+
+        workbook = xlsxwriter.Workbook('all_links.xlsx')
+
+        worksheet = workbook.add_worksheet('data')
+
+        i_row = 0
+
+        worksheet.write(i_row, 0, 'links')
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103'
+        }
+
+        for i in range(0, 2):
+            url = "https://fr.indeed.com/jobs?q=informatique&l=La%20R%C3%A9union&sc=0kf%3Ajt(permanent)%3B&start=" \
+                  + str(i * 10)
+
+            print('url : ' + url)
+
+            # Request the content of a page from the url
+            html = requests.get(url, headers=headers)
+
+            time.sleep(7)
+
+            # Parse the content of html_doc
+            soup = BeautifulSoup(html.content, 'html.parser')
+
+            if soup.find("h2", {"class": "jobTitle"}) is not None:
+                all_h2 = soup.find_all("h2", {"class": "jobTitle"})
+
+                for h2 in all_h2:
+                    link = "https://fr.indeed.com" + h2.find('a').get('href')
+                    i_row += 1
+                    worksheet.write(i_row, 0, link)
+                    print('link : ' + link)
+            else:
+                print("no ads")
+
+        workbook.close()
+
 
 if __name__ == '__main__':
     unittest.main()
