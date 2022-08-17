31514d90b9d9ce0ee99c139ce440483755793812
Jason ALOYAU <jason.aloyau@outlook.fr>
2022-08-17 19:38:14 +0000

    AI _ KM _ DM _ Library _ Wikipedia _ Chemistry _ Chemicals _ unit_tests_for_github.py _ UnitTestsDataMiningWikipediaChemistryChemicals _ test_extract_the_abundance_of_elements_in_earth_crust

--- Artificial_Intelligence/Knowledge_Management/Data_Mining/Library/Wikipedia/Chemistry/Chemicals/unit_tests_for_github.py
+++ Artificial_Intelligence/Knowledge_Management/Data_Mining/Library/Wikipedia/Chemistry/Chemicals/unit_tests_for_github.py
@@ -91,6 +91,75 @@ class UnitTestsDataMiningWikipediaChemistryChemicals(unittest.TestCase):
         else:
             print("no tbody")
 
+    # ok
+    def test_extract_the_abundance_of_elements_in_earth_crust(self):
+        print('test_extract_the_abundance_of_elements_in_earth_crust')
+
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103'
+        }
+
+        url = "https://en.wikipedia.org/wiki/Abundance_of_elements_in_Earth%27s_crust"
+
+        # Request the content of a page from the url
+        html = requests.get(url, headers=headers)
+
+        # print(html.content)
+
+        time.sleep(3)
+
+        # Parse the content of html_doc
+        soup = BeautifulSoup(html.content, 'html.parser')
+
+        data = []
+
+        if soup.find_all("tbody")[0] is not None:
+            all_tr = soup.find_all("tbody")[0].find_all("tr")
+
+            for tr in all_tr:
+                if tr.find('td'):
+                    z = tr.find_all('td')[0].text.replace(',', '').replace('\n', '')
+
+                    name = tr.find_all('td')[1].text.replace(',', '').replace('\n', '')
+
+                    symbol = tr.find_all('td')[2].text.replace(',', '').replace('\n', '')
+
+                    darling = tr.find_all('td')[3].text.replace(',', '').replace('\n', '')
+
+                    barbalace = tr.find_all('td')[4].text.replace(',', '').replace('\n', '')
+
+                    webelements = tr.find_all('td')[5].text.replace(',', '').replace('\n', '')
+
+                    israel_science_and_technology = tr.find_all('td')[6].text.replace(',', '').replace('\n', '')
+
+                    crc = tr.find_all('td')[7].text.replace(',', '').replace('\n', '')
+
+                    annual_production_by_extraction_in_2016_in_tonnes = tr.find_all('td')[8].text.replace(',', '')\
+                        .replace('\n', '')
+
+                    data_element = {
+                        'element': {
+                            'z': z,
+                            'name': name,
+                            'symbol': symbol
+                        },
+                        'abundance_by_source_in_ppm': {
+                            'darling': darling,
+                            'barbalace': barbalace,
+                            'webelements': webelements,
+                            'israel_science_and_technology': israel_science_and_technology,
+                            'crc': crc
+                        },
+                        'annual_production_by_extraction_in_2016_in_tonnes':
+                            annual_production_by_extraction_in_2016_in_tonnes
+                    }
+
+                    data.append(data_element)
+
+                    print(str(data_element) + ",")
+        else:
+            print("no tbody")
+
 
 if __name__ == '__main__':
     unittest.main()
